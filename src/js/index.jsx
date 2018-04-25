49aeb52f64f8e5f438a8de6d45fff289485aaf7f
Kasiriveni <kasiriveni@gmail.com>
2018-04-25 19:48:45 +0530

    eslint added
    Added jsx support
    fixied eslint
    
    Co-Authored-By: Thara <tharaks727@gmail.com>
    Co-Authored-By: Soubhagya Jena <soubhagya-ui@users.noreply.github.com>

---
+++ src/js/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Welcome from '../components/welcome.jsx';
+import '../css/style.css';
+
+const root = document.getElementById('root');
+
+ReactDOM.render(
+  <Welcome />,
+  root,
+);
+
