80ab1458ee2dcdc8f9ae14839493e2fe2d9f7a2a
Soubhagya <soubhagya.fukuli@gmail.com>
2018-05-04 15:56:01 +0530

    Adding div
    Adding some divs for testing

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 class Welcome extends React.Component {
   render() {
     return (
-      <div>hello</div>
+      <div>
+        <div>hello</div>
+        <div>hello</div>
+        <div>hello</div>
+      </div>
     );
   }
 }
