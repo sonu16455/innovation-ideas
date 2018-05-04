e2b3ffc7f5d931fa43fd133342de3c51a12d8da1
Soubhagya <soubhagya.fukuli@gmail.com>
2018-05-04 17:55:05 +0530

    issue resolved
    issue resolved . Please verify

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 class Signup extends React.Component {
-    constructor(props){
-        super(props);
-    }
+    
     render(){
         return(
             <React.Fragment>
