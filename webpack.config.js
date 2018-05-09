4fcc61aebc92b5ff5c880bb09217321df2a534ea
Kasiriveni <kasiriveni@gmail.com>
2018-05-09 11:37:06 +0530

    adding the prettier
    added support for code formatter

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,32 @@
-const path=require('path');
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
-module.exports={
-    entry:'./src/js/index.jsx',
-    output:{
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          {
-						test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-        },
-          {
-            test: /\.css$/,
-             use: [ 'style-loader', 'css-loader' ]
-          },
-          { test: /\.(png|woff|woff2|eot|ttf|svg|jpeg|jpg)$/, loader: 'url-loader?limit=100000' }
-        ]
-      },
-      devServer: {
-        port: 8000,
-        historyApiFallback: true
-      },
-}
+module.exports = {
+	entry: "./src/js/index.jsx",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js"
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				loader: "babel-loader"
+			},
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"]
+			},
+			{
+				test: /\.(png|woff|woff2|eot|ttf|svg|jpeg|jpg)$/,
+				loader: "url-loader?limit=100000"
+			}
+		]
+	},
+	devServer: {
+		port: 8000,
+		historyApiFallback: true
+	},
+	plugins: [new CleanWebpackPlugin("./dist")]
+};
